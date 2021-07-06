9bfcee597149c8330dc5f0916c3007a6f6daa501
Matheus Pilon <mbpilon@gmail.com>
2021-07-06 09:18:51 -0300

    finish

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ black }) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://occ-0-1316-185.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABWu33TcylnaLZwSdtgKR6mr0O63afqQLxZbzHYQZLkCJ9bgMTtsf6tzs_ua2BuTpAVPbhxnroiEA-_bqJmKWiXblO9h-.png?r=f71" />
+                </a>
+            </div>
+        </header>
+    )
+}
